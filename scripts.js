edc2f7d2eddc0e5c878d418c302669ae3e23c866
Miko <mikolaasanen@hotmail.com>
2024-08-14 16:47:49 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -123,8 +123,8 @@ function search() {
   
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
+      button = li[i].getElementsByTagName("button")[0];
+      txtValue = button.textContent || button.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         li[i].style.display = "";
       } else {
