1b8cbcdb6a8c03baf766de2c6b53f2aec9a8674c
Miko <mikolaasanen@hotmail.com>
2024-08-14 16:44:33 +0300

    Automated commit of new HTML pages

--- scripts.js
+++ scripts.js
@@ -112,3 +112,23 @@ function toggle_auto() {
     let button = document.getElementById('auto');
     button.style.backgroundColor = auto ? '#52a25f' : '#a44444';
 }
+
+function search() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName("list")[0];
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
